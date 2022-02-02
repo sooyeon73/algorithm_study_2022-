77d6aa07e10bacd871938744335a4d05a065fd5f
hanby <hanby.choi@ewhain.net>
2022-02-02 23:17:40 +0900

    2743 단어 길이 재기 최한비

---
+++ 3주차_기초203/2743_hb.java
@@ -0,0 +1,13 @@
+//2743: 203 - 자료구조 1 (참고) 단어 길이 재기
+import java.util.Scanner;
+
+public class WordLength {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		System.out.println(word.length());
+		sc.close();
+		return;
+	}
+}
