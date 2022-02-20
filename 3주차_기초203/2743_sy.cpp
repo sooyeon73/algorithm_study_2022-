8d0dc88e8a1cbd236bb97786f1e939b21f8b8e87
sooyeon73 <73420533+sooyeon73@users.noreply.github.com>
2022-02-20 19:28:37 +0900

    2743 단어길이재기 박수연

---
+++ 3주차_기초203/2743_sy.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    string input;
+    cin>>input;
+    cout<<input.length();
+    return 0;
+}
