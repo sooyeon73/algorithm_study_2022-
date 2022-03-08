a80f593a3fbfabb9bdc2d6591a7d44dd16dad769
sooyeon73 <73420533+sooyeon73@users.noreply.github.com>
2022-03-08 15:46:16 +0900

    15990 123더하기5 박수연

--- 5주차_기초400_다이나믹프로그래밍/15990_sy.cpp
+++ 5주차_기초400_다이나믹프로그래밍/15990_sy.cpp
@@ -1,5 +1,4 @@
 //long long 사용해야
-//더할 때마다 mod 연산
 
 #include<iostream>
 #include<vector>
@@ -15,12 +14,11 @@ int main (){
         if(n[i]>max)max=n[i];
     }
     
-    vector<long long int> d(max+1);
     vector<tuple<long long int,long long int,long long int>> last(max+1);
     
-    d[1]=1; last[1]=make_tuple(1,0,0); //마지막 숫자가 123인 횟수 저장
-    d[2]=2; last[2]=make_tuple(0,1,0);
-    d[3]=3; last[3]=make_tuple(1,1,1);
+    last[1]=make_tuple(1,0,0); //마지막 숫자가 123인 횟수 저장
+    last[2]=make_tuple(0,1,0);
+    last[3]=make_tuple(1,1,1);
     
     for(int i=4 ; i<=max ; i++){
         //마지막 숫자가 본인이 아닌 경우에만 +
@@ -31,8 +29,7 @@ int main (){
 
 
     for(int i=0; i<t;i++){
-        cout<<((((get<0>(last[n[i]])+get<1>(last[n[i]]))%1000000009)+get<2>(last[n[i]]))%1000000009)<<"\n";
-        //+마다 mod연산
+        cout<<(((get<0>(last[n[i]])+get<1>(last[n[i]]))+get<2>(last[n[i]]))%1000000009)<<"\n";
     }
     return 0;
 }
