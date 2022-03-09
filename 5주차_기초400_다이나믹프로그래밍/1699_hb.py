4869414ea81d877a2ff1a0b0fe55fa586124d561
hanby <hanby.choi@ewhain.net>
2022-03-09 22:14:40 +0900

    1699 제곱수의 합 최한비

--- 5주차_기초400_다이나믹프로그래밍/1699_hb.py
+++ 5주차_기초400_다이나믹프로그래밍/1699_hb.py
@@ -1,10 +1,10 @@
 # 1699: 400 - 다이나믹 프로그래밍 1 - 제곱수의 합
 n = int(input())
-dp = [0 for _ in range(n+1)]
+dp = [i for i in range(n+1)] # for문 두 번 돌리지 말고 여기서 초기화까지 하기
 for i in range(1,n+1):
-    dp[i] = i
     for j in range(1,i):
-        if(j**2)>i:
+        if(j*j)>i: # j**2 대신 j*j로 계산해야 함 (시간초과)
             break
-        dp[i] = min(dp[i], dp[i-j**2]+1)
+        if dp[i] > dp[i - j * j] + 1:
+            dp[i] = dp[i - j * j] + 1 # min 함수를 사용하면 시간초과됨
 print(dp[n])
