bc1912ecf5c80c7237800c1a2f09f5c325bdde90
hanby <hanby.choi@ewhain.net>
2022-03-27 20:33:38 +0900

    1476 날짜 계산 최한비

--- 7주차_기초500510_브루트포스/1476_hb.py
+++ 7주차_기초500510_브루트포스/1476_hb.py
@@ -1,19 +1,11 @@
 # 1476: 500 - 브루트 포스 - 날짜 계산
 E, S, M = map(int, input().split())
-year = [[[0]*20 for j in range(29)] for i in range(16)]
-max = 15*28*19
-i, j, k = 1, 1, 1
-for result in range(1, max+1):
-    year[i][j][k] = result
-    i += 1
-    j += 1
-    k += 1
-    if i == 16:
-        i = 1
-    if j == 29:
-        j = 1
-    if k == 20:
-        k = 1
-    if year[E][S][M] != 0:
+i, j, k, count = 1, 1, 1, 1
+while(1):
+    if i == E and j == S and k == M:
+        print(count)
         break
-print(year[E][S][M])
+    i += 1; j += 1; k += 1; count += 1
+    if i == 16: i = 1
+    if j == 29: j = 1
+    if k == 20: k = 1
