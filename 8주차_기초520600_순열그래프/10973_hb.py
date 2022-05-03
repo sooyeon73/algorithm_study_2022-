9bde0b7a8fb0caba9c734fa883d788b7135b9514
Hanby Choi <hanby.choi@ewhain.net>
2022-05-03 11:25:14 +0900

    10973 이전 순열 최한비

---
+++ 8주차_기초520600_순열그래프/10973_hb.py
@@ -0,0 +1,17 @@
+# 10973: 520 - 브루트 포스 - 순열 - 이전 순열
+n = int(input())
+arr = list(map(int, input().split()))
+find = False
+for i in range(n-1, 0, -1):
+    if arr[i-1] > arr[i]:
+        for j in range(n-1, 0, -1):
+            if arr[i-1] > arr[j]:
+                arr[i-1], arr[j] = arr[j], arr[i-1] # swap
+                arr = arr[:i] + sorted(arr[i:], reverse=True) # i-1까지는 swap한 대로, i부터는 내림차순 sort
+                find = True
+                break
+    if find == True:
+        print(*arr) # 리스트 앞에 *를 붙이면 안에 있는 숫자들을 1 2 3 4 이런 식으로 출력할 수 있음
+        break
+if not find:
+    print(-1)
